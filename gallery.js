7f0e7428df196d24c89262d6982b141ef5059741
FearlessJeny <161617854+FearlessJeny@users.noreply.github.com>
2026-07-25 22:09:02 +0300

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,69 @@
+const buttons = document.querySelectorAll(".categories button");
+const items = document.querySelectorAll(".item");
+
+
+buttons.forEach(button => {
+
+button.addEventListener("click",()=>{
+
+
+let filter = button.dataset.filter;
+
+
+items.forEach(item=>{
+
+
+if(filter==="all" || item.classList.contains(filter)){
+
+item.style.display="block";
+
+}
+
+else{
+
+item.style.display="none";
+
+}
+
+
+});
+
+
+});
+
+
+});
+
+
+
+
+
+const images = document.querySelectorAll(".item img");
+const popup = document.querySelector(".popup");
+const popupImg = document.querySelector(".popup img");
+
+
+
+images.forEach(img=>{
+
+
+img.onclick=()=>{
+
+
+popup.classList.add("active");
+
+popupImg.src=img.src;
+
+
+}
+
+
+});
+
+
+
+popup.onclick=()=>{
+
+popup.classList.remove("active");
+
+}
