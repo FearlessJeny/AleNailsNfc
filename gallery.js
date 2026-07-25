c5fb302034747271291ac166da8b5bd318528283
FearlessJeny <161617854+FearlessJeny@users.noreply.github.com>
2026-07-25 23:14:31 +0300

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -1,69 +1,108 @@
-const buttons = document.querySelectorAll(".categories button");
-const items = document.querySelectorAll(".item");
-
-
-buttons.forEach(button => {
-
-button.addEventListener("click",()=>{
-
-
-let filter = button.dataset.filter;
-
-
-items.forEach(item=>{
-
-
-if(filter==="all" || item.classList.contains(filter)){
-
-item.style.display="block";
-
-}
-
-else{
-
-item.style.display="none";
-
-}
-
-
-});
-
-
-});
-
-
-});
-
-
-
-
-
-const images = document.querySelectorAll(".item img");
 const popup = document.querySelector(".popup");
 const popupImg = document.querySelector(".popup img");
+const popupTitle = document.querySelector(".popup h2");
+
+const allImages = document.querySelectorAll(".item img");
+
+let current = 0;
 
 
 
-images.forEach(img=>{
+allImages.forEach((img,index)=>{
 
+    img.onclick = () => {
 
-img.onclick=()=>{
+        current = index;
 
+        openImage();
 
-popup.classList.add("active");
-
-popupImg.src=img.src;
-
-
-}
-
+    }
 
 });
 
 
 
-popup.onclick=()=>{
+function openImage(){
 
-popup.classList.remove("active");
+    popup.classList.add("active");
+
+    popupImg.src = allImages[current].src;
+
+    popupTitle.innerHTML =
+    allImages[current]
+    .parentElement
+    .querySelector("h3")
+    .innerHTML;
 
 }
+
+
+
+document.querySelector(".next").onclick = () => {
+
+    current++;
+
+    if(current >= allImages.length){
+        current = 0;
+    }
+
+    openImage();
+
+};
+
+
+
+document.querySelector(".prev").onclick = () => {
+
+    current--;
+
+    if(current < 0){
+        current = allImages.length - 1;
+    }
+
+    openImage();
+
+};
+
+
+
+document.querySelector(".close").onclick = () => {
+
+    popup.classList.remove("active");
+
+};
+
+
+
+// Свайп пальцем на телефоне
+
+let startX = 0;
+
+
+popup.addEventListener("touchstart", e => {
+
+    startX = e.touches[0].clientX;
+
+});
+
+
+popup.addEventListener("touchend", e => {
+
+    let endX = e.changedTouches[0].clientX;
+
+
+    if(startX - endX > 50){
+
+        document.querySelector(".next").click();
+
+    }
+
+
+    if(endX - startX > 50){
+
+        document.querySelector(".prev").click();
+
+    }
+
+
+});
