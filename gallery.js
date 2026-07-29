f242790ee38f708331c1f52f76cf7036b822a2ca
FearlessJeny <161617854+FearlessJeny@users.noreply.github.com>
2026-07-29 15:26:06 +0300

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -3,138 +3,25 @@
 const buttons = document.querySelectorAll(".categories button");
 const items = document.querySelectorAll(".item");
 
-
 buttons.forEach(button => {
 
     button.addEventListener("click", () => {
 
         const filter = button.dataset.filter;
 
-
         items.forEach(item => {
 
-            if(filter === "all" || item.classList.contains(filter)){
-
+            if (filter === "all" || item.classList.contains(filter)) {
                 item.style.display = "block";
-
             } else {
-
                 item.style.display = "none";
-
             }
 
         });
 
-    });
-
-});
-
-
-
-
-// ОКНО ФОТО
-
-const popup = document.querySelector(".popup");
-const popupImg = document.querySelector(".popup img");
-const popupTitle = document.querySelector(".popup h2");
-
-const images = document.querySelectorAll(".item img");
-
-let current = 0;
-
-
-
-function openPhoto(){
-
-    popup.classList.add("active");
-
-    popupImg.src = images[current].src;
-
-    popupTitle.textContent =
-    images[current]
-    .parentElement
-    .querySelector("h3")
-    .textContent;
-
-}
-
-
-
-// открытие фото
-
-images.forEach((img,index)=>{
-
-    img.addEventListener("click",()=>{
-
-        current = index;
-
-        openPhoto();
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
 
     });
 
 });
-
-
-
-// закрыть
-
-document.querySelector(".close")
-.addEventListener("click",()=>{
-
-    popup.classList.remove("active");
-
-});
-
-
-
-// следующая фотография
-
-document.querySelector(".next")
-.addEventListener("click",()=>{
-
-    current++;
-
-    if(current >= images.length){
-
-        current = 0;
-
-    }
-
-    openPhoto();
-
-});
-
-
-
-// предыдущая фотография
-
-document.querySelector(".prev")
-.addEventListener("click",()=>{
-
-    current--;
-
-    if(current < 0){
-
-        current = images.length - 1;
-
-    }
-
-    openPhoto();
-
-});
-
-
-
-// свайп телефона
-
-let startX = 0;
-
-
-popup.addEventListener("touchstart",(e)=>{
-
-    startX = e.touches[0].clientX;
-
-});
-
-
-
